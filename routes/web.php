b450550bab3cdbb7c41e61405818427e16c76a57
Abhishek <abhi242singh@gmail.com>
2024-06-19 23:26:05 +0530

    show page

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-// Route::get('/', function () {
-//     return view('particls.first');
-// });
+Route::get('/', function () {
+    return view('particls.first');
+});
 
 
 
-Route::get('/', [UserController::class, 'showUserWithAddresses']);
+// Route::get('/', [UserController::class, 'showUserWithAddresses']);
 Route::get('/send-login-mail', [UserController::class, 'sendLoginMail']);
 
